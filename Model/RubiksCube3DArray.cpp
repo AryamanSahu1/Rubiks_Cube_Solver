8fdd0a18ef835352986d174187ca5a8dd1380f8f
Aryaman Sahu <aryansahu7999@gmail.com>
2026-06-27 21:19:21 +0530

    Added 1D array Representaion of Rubik's Cube

--- Model/RubiksCube3DArray.cpp
+++ Model/RubiksCube3DArray.cpp
@@ -235,7 +235,7 @@ public:
 
 struct Hash3d {
     size_t operator()(const RubiksCube3DArray &r1) const {
-        string str = "";
+        string str="";
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 3; j++) {
                 for (int k = 0; k < 3; k++) {
